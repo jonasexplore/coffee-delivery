22df8a430535b959b7b9cabc1339c3ef9ced6020
Jonas <jonasexplore@gmail.com>
2022-11-06 16:53:00 -0300

    chore: added icons

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,11 @@
+import cart from './ShoppingCartSimple.svg';
+import logo from './Logo.svg';
+import spot from './Spot.svg';
+
+const icons = {
+  cart,
+  logo,
+  spot,
+};
+
+export default icons;
