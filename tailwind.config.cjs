4b47f94b6b9413ceb25665b75956aa4f9eabc2b8
Jonas Brito <jonasexplore@gmail.com>
2022-11-12 12:48:14 -0300

    chore: configured theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        home: "url('./background.png')",
+      },
+      colors: {
+        'yellow-light': '#F1E9C9',
+        'yellow-medium': '#DBAC2C',
+        'yellow-dark': '#C47F17',
+        'purple-light': '#EBE5F9',
+        'purple-medium': '#8047F8',
+        'purple-dark': '#4B2995',
+        current: 'currentColor',
+      },
+      width: {
+        17: '4.25rem',
+        18: '4.5rem',
+      },
+      fontFamily: {
+        baloo: ["'Baloo 2'"],
+        sans: ['Roboto', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
