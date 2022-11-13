38f340a91d99cbfcd82a362fef2645f7a915a350
Jonas Brito <jonasexplore@gmail.com>
2022-11-13 08:48:16 -0300

    chore: added new icons

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,9 +1,21 @@
 import logo from './Logo.svg';
 import spot from './Spot.svg';
+import trash from './trash.svg';
+import mark from './mark.svg';
+import dollar from './dollar-sign.svg';
+import creditCard from './credit-card.svg';
+import build from './build.svg';
+import money from './money.svg';
 
 const icons = {
   logo,
   spot,
+  trash,
+  mark,
+  dollar,
+  creditCard,
+  build,
+  money,
 };
 
 export default icons;
