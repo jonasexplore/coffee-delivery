77bb38b4240ee6efbe3cdedf8a76bf81597d7831
Jonas Brito <jonasexplore@gmail.com>
2022-11-13 09:12:55 -0300

    feat: added checkout route

--- src/router.tsx
+++ src/router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home } from './pages';
+import { HomePage } from './pages';
+import { CheckoutPage } from './pages/checkout';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
     </Routes>
   );
 };
