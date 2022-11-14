24dcf10476718f903ff44ec53468bbfeb85222fe
Jonas Brito <jonasexplore@gmail.com>
2022-11-14 09:47:40 -0300

    feat: added success route

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+
 import { HomePage } from './pages';
 import { CheckoutPage } from './pages/checkout';
+import { SuccessPage } from './pages/success';
 
 export const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path="/success/:id" element={<SuccessPage />} />
     </Routes>
   );
 };
